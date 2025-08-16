7a95edd2c8066888bb4d6331e35e1dd7f57ec481
chshubh <chshubh091@gmail.com>
2025-08-16 07:23:42 +0000

    datattype

--- .devcontainer/datatypes-summary.js
+++ .devcontainer/datatypes-summary.js
@@ -1,26 +1,41 @@
 //PRIMITIVE
 
 // 7 CATEGORY
-//: STRING,NUMBER,BOOLEAN,NULL, UNDEFINED,SYMBOL,BIGINT
+// STRING,NUMBER,BOOLEAN,NULL, UNDEFINED,SYMBOL,BIGINT
 
-const score = 100
-const scoreVlue = 100.3
-const IsLoggedIn = false
-const outsideTemp = null
-let userEmail;
-const id = symbol('123')
-const AnotherId= symbol('123')
-const bigNumber = 23726237642463246753242374743742n
+// const score = 100
+// const scoreVlue = 100.3
+// const IsLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
+// const id = symbol('123')
+// const AnotherId= symbol('123')
+// const bigNumber = 23726237642463246753242374743742n
 
-//REFERENCE TYPE(NON PRIMITIVE)
+// //REFERENCE TYPE(NON PRIMITIVE)
 
-//ARRAYS,OBJECTS,FUNCTIONS
+// //ARRAYS,OBJECTS,FUNCTIONS
 
-const heros =["shaktiman" , "naagraj", "dogesh"] // arrays
-let myObj={
-    name:"shubham", age: 18,
+// const heros =["shaktiman" , "naagraj", "dogesh"] // arrays
+// let myObj={
+//     name:"shubham", age: 18,
+// }
+//  const myFunction = function(){console.log('HELLO WORLD  !!!');
+//  }
+//    console.log(typeof bigNumber); //USED TO CHECK THE DATATYPE OF THE DATA
+
+
+//***********************************************************************************************
+//  Stack(primitive),Heap(non-primitive)
+let myYoutubename = "lakshya"
+let anotherName = "myYoutubename"
+console.log(anotherName);
+
+let userone = {
+    email:"user@google.com"
+
+
 }
- const myFunction = function(){console.log('HELLO WORLD  !!!');
- }
-   console.log(typeof bigNumber); //USED TO CHECK THE DATATYPE OF THE DATA
-   
+let usertwo = userone
+usertwo.email="shivam@google.com"
+HTMLFormControlsCollection.lo("usertwo");
