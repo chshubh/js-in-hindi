639ccb51161fdfe5a500fc4a28994ff02349d914
chshubh <chshubh091@gmail.com>
2025-08-16 07:08:45 +0000

    datatypes

---
+++ .devcontainer/datatypes-summary.js
@@ -0,0 +1,26 @@
+//PRIMITIVE
+
+// 7 CATEGORY
+//: STRING,NUMBER,BOOLEAN,NULL, UNDEFINED,SYMBOL,BIGINT
+
+const score = 100
+const scoreVlue = 100.3
+const IsLoggedIn = false
+const outsideTemp = null
+let userEmail;
+const id = symbol('123')
+const AnotherId= symbol('123')
+const bigNumber = 23726237642463246753242374743742n
+
+//REFERENCE TYPE(NON PRIMITIVE)
+
+//ARRAYS,OBJECTS,FUNCTIONS
+
+const heros =["shaktiman" , "naagraj", "dogesh"] // arrays
+let myObj={
+    name:"shubham", age: 18,
+}
+ const myFunction = function(){console.log('HELLO WORLD  !!!');
+ }
+   console.log(typeof bigNumber); //USED TO CHECK THE DATATYPE OF THE DATA
+   
