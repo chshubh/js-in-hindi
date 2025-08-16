81c3b4361f349b553897b8390036562f54f86faa
chshubh <chshubh091@gmail.com>
2025-08-16 05:01:28 +0000

    dtattypes

--- .devcontainer/01-BASICS/01_variables.js
+++ .devcontainer/01-BASICS/01_variables.js
@@ -2,6 +2,14 @@ const accountId = 144559
 let accountEmail = "hitesh@google.com"
 var accountPassword = "12345"
 accountCity = "Jaipur"
+let accountState ;
 
-
+// accountId = "2" // not allowed
 console.log(accountId);
+accountEmail = "hc@hcc.com"
+accountPassword = "232312"
+accountCity = "kolkata"
+console.table([accountId,accountEmail,accountPassword,accountCity]);
+/* 
+comment:prefer not to use var, because of issue in block scope and functional scope
+*/
